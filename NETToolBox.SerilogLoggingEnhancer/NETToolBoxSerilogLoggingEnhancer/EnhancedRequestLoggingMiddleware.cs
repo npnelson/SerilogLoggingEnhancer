5fe636f843e1509de28cc4f3b9fe8435519cd204
npnelson <nnelson1@insight.rr.com>
2020-01-17 11:20:20 -0500

    wip

---
+++ NETToolBox.SerilogLoggingEnhancer/NETToolBoxSerilogLoggingEnhancer/EnhancedRequestLoggingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using NETToolBoxSerilogLoggingEnhancer;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace NETToolBoxSerilogLoggingEnhancer
+{
+    public class EnhancedRequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IDiagnosticContext _diagnosticContext;
+        private readonly string _envName;
+        public EnhancedRequestLoggingMiddleware(RequestDelegate next, IWebHostEnvironment env, IDiagnosticContext diagnosticContext)
+        {
+            _diagnosticContext = diagnosticContext;
+            _envName = env.EnvironmentName;
+            _next = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            _diagnosticContext.Set("EnvironmentName", _envName);
+            await _next(httpContext);
+        }
+    }
+}
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class EnhancedRequestLoggingMiddlewareExtensions
+    {
+        /// <summary>
+        /// Adds several opinionated properties to the SerilogRequestLoggingMiddleware
+        /// </summary>
+        /// <typeparam name="T">Any type from the assembly version that to be reported in ApplicationVersion, typically the type of your startup class</typeparam>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseEnhancedRequestLogging<T>(this IApplicationBuilder builder)
+        {
+            return builder.UseSerilogRequestLogging(opts => opts.EnrichDiagnosticContext = LogHelper<T>.EnrichFromRequest).UseMiddleware<EnhancedRequestLoggingMiddleware>();
+        }
+
+    }
+}
---
+++ NETToolBox.SerilogLoggingEnhancer/NETToolBoxSerilogLoggingEnhancer/LogHelper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using NETToolBox.LinuxVersion;
+using Serilog;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+namespace NETToolBoxSerilogLoggingEnhancer
+{
+    internal static class LogHelper<T>
+    {
+        private static readonly string? _version = typeof(T).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+        public static void EnrichFromRequest(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+        {
+            var request = httpContext.Request;
+
+            // Set all the common properties available for every request
+            diagnosticContext.Set("Host", request.Host);
+            diagnosticContext.Set("Protocol", request.Protocol);
+            diagnosticContext.Set("Scheme", request.Scheme);
+            diagnosticContext.Set("CLRVersion", RuntimeInformation.FrameworkDescription);
+            diagnosticContext.Set("RemoteIP", httpContext.Features.Get<IHttpConnectionFeature>().RemoteIpAddress);
+            diagnosticContext.Set("UserAgent", httpContext.Request.Headers.FirstOrDefault(x => x.Key == "User-Agent").Value);
+            diagnosticContext.Set("OSVersion", RuntimeInformation.OSDescription);
+            diagnosticContext.Set("ApplicationVersion", _version);
+            diagnosticContext.Set("MachineName", Environment.MachineName);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                diagnosticContext.Set("LinuxVersion", GetLinuxVersion.GetLinuxVersionInfo().VersionString);
+            }
+            // Only set it if available. You're not sending sensitive data in a querystring right?!
+            if (request.QueryString.HasValue)
+            {
+                diagnosticContext.Set("QueryString", request.QueryString.Value);
+            }
+
+            // Set the content-type of the Response at this point
+            diagnosticContext.Set("ContentType", httpContext.Response.ContentType);
+
+            // Retrieve the IEndpointFeature selected for the request
+            var endpoint = httpContext.GetEndpoint();
+            if (endpoint is object) // endpoint != null
+            {
+                diagnosticContext.Set("EndpointName", endpoint.DisplayName);
+            }
+        }
+    }
+}
